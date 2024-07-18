2083900ea7d2e4ad493810c6486c0c785e073895
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-18 16:29:20 -0600

    Feat/Test:ship object and testing for initialize

---
+++ spec/ship_spec.rb
@@ -0,0 +1,21 @@
+require './lib/ship'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+    end
+
+RSpec.describe 'Ship' do
+    before(:each) do
+        @cruiser = Ship.new("Cruiser", 3)
+    end
+
+    it 'exists' do
+        expect(@cruiser).to be_a(Ship)
+    end
+
+    it 'has a name and length' do
+        expect(@cruiser.name).to eq("Cruiser")
+        expect(@cruiser.length).to eq(3)
+    end
+ 
+end
