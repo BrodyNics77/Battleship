11010786af846addac56da082f6275e80e566206
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-18 19:18:45 -0500

    feat: coordinate & ship test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,18 @@
 class Cell
 
+    attr_reader :coordinate, :ship
+
+    def initialize(coordinate)
+        @coordinate = coordinate
+        @ship = nil
+    end
+
+    def empty?
+        if @ship == nil
+            true
+        else false
+        end
+    end
+
+    
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,8 +6,28 @@ RSpec.configure do |config|
     end
 
 RSpec.describe 'Cell' do
+    before(:each) do
+        @cell = Cell.new("B4")
+    end
 
+    it 'exists' do
+        expect(@cell).to be_a(Cell)
+    end
 
+    it 'has a coordinate' do 
+        expect(@cell.coordinate).to eq("B4")
+    end
 
+    describe 'ship on a cell' do
+        it 'returns nil by default' do
+        expect(@cell.ship).to eq nil
+        end
 
+        it 'is empty when nil' do
+            @cell.empty?
+            expect(@cell.empty?).to be true
+        # cruiser = Ship.new("Cruiser", 3)
+        end
+        
+    end
 end
