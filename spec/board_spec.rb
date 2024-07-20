455e3b3d226de5db53d603a6f70f9ed1c28427b7
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-20 12:51:44 -0600

    Test: further testing for board cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,35 @@ RSpec.configure do |config|
     end
 
 RSpec.describe 'Board' do
-    
+    before(:each) do
+        @board = Board.new
+    end
+    it 'exists' do
+        expect(@board).to be_a(Board)
+    end
+
+    it 'has cells stored in a hash' do
+        expect(@board.cells).to be_a(Hash)
+    end
+
+    it 'has a total of 16 key/value pairs in the hash' do
+        @board.cells
+
+        expect(@board.cells).to eq()
+    end
+
+    it 'has the cells keys as coordinate strings' do
+        @board.cells
+
+    end
+
+    it 'has the values as cell objects' do
+        @board.cells
+
+    end
+
+
 end
+# The Board class is responsible for keeping track of cells, 
+# validating coordinates, validating ship placements, placing ships, 
+# and rendering a visual representation of itself.
