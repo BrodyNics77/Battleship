1b43fdb5294df47cd0925749dba6a527155d96ee
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-21 13:26:36 -0500

    test: working on valid_placement?

--- lib/board.rb
+++ lib/board.rb
@@ -3,32 +3,36 @@ class Board
     attr_reader :cells
 
     def initialize
-        @cells = loc
+        @cells = location
     end
 
-    def loc
-        cells = {
-            "A1" => @cell,
-            "A2" => @cell,
-            "A3" => @cell,
-            "A4" => @cell,
-            "B1" => @cell,
-            "B2" => @cell,
-            "B3" => @cell,
-            "B4" => @cell,
-            "C1" => @cell,
-            "C2" => @cell,
-            "C3" => @cell,
-            "C4" => @cell,
-            "D1" => @cell,
-            "D2" => @cell,
-            "D3" => @cell,
-            "D4" => @cell,
+    def location
+        loc = {
+            "A1" => Cell.new('A1'),
+            "A2" => Cell.new('A2'),
+            "A3" => Cell.new('A3'),
+            "A4" => Cell.new('A4'),
+            "B1" => Cell.new('B1'),
+            "B2" => Cell.new('B2'),
+            "B3" => Cell.new('B3'),
+            "B4" => Cell.new('B4'),
+            "C1" => Cell.new('C1'),
+            "C2" => Cell.new('C2'),
+            "C3" => Cell.new('C3'),
+            "C4" => Cell.new('C4'),
+            "D1" => Cell.new('D1'),
+            "D2" => Cell.new('D2'),
+            "D3" => Cell.new('D3'),
+            "D4" => Cell.new('D4'),
         }
     end
 
     def valid_coordinate?(coordinate)
         @cells.key?(coordinate)
+    end
+
+    def valid_placement?()
+
     end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,14 +23,49 @@ RSpec.describe 'Board' do
             expect(@board.cells.size).to eq(16)
         end
     
-        it 'cells have key/val pair as coordinate strings' do
+        it '#cells have key/val pair as coordinate strings' do
             expect(@board.valid_coordinate?("A1")).to be true
             expect(@board.valid_coordinate?("D1")).to be true
             expect(@board.valid_coordinate?("A5")).to be false
             expect(@board.valid_coordinate?("E1")).to be false
             expect(@board.valid_coordinate?("A22")).to be false
+        end        
+    end
+
+    describe 'validating ship placements' do
+        xit '#valid_placement? of a ships length' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2)
+
+            expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+            expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
         end
-        
+
+        xit '#valid_placement? of consecutive coordinates' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2)
+
+            expect(@board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+            expect(@board.valid_placement?(submarine, ["A1", "C1"])).to be false
+            expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+            expect(@board.valid_placement?(submarine, ["C1", "B1"])).to be false
+        end
+
+        xit '#valid_placement? coordinates cannot go diagonal' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2)
+
+            expect(@board.valid_placement?(cruiser, ["A1", "B2", "C4"])).to be false
+            expect(@board.valid_placement?(submarine, ["C2", "D3"])).to be false
+        end
+
+        xit '#valid_placement? should be true' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2)
+
+            expect(@board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+            expect(@board.valid_placement?(submarine, ["A1", "A2"])).to be true
+        end
     end
 
 end
